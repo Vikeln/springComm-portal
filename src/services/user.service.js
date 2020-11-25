a086341e694c4bc5304496d1a5a7e4863d0979ba
KThuku <KThuku@mfs.co.ke>
2020-11-25 14:45:29 +0300

    added helper on timeToSend input and changed URLs

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,12 +1,12 @@
 import { axiosInstance } from '../API';
 
 
-// const userUrl = "http://10.38.83.54:30555/users";
-// const baseUrl = "http://10.38.83.54:30555/";
+const userUrl = "http://10.38.83.54:30555/users";
+const baseUrl = "http://10.38.83.54:30555/";
 
 
-const userUrl = "http://localhost:8080/users";
-const baseUrl = "http://localhost:8080/";
+// const userUrl = "http://localhost:8080/users";
+// const baseUrl = "http://localhost:8080/";
 
 
 class UserService {
